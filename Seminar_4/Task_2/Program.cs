c27a493f8cc9376e58ad74cd387e9aea14bc31ee
armoney7 <112188406+armoney7@users.noreply.github.com>
2023-02-26 17:13:11 +0700

    Семинар 4 Задание 2

---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int number1 = 0;
+int Num(int number)
+{
+    while (number > 0)
+    {
+        number1 = number % 10;
+        sum += number1;
+        number /= 10;
+    }
+    return sum;
+}
+int result = Num(num);
+Console.Write($"Сумма цифр числа {num} -> {result}");
