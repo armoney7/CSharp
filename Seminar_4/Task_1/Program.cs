3556c07650acf01969ddc7dcbe842d01cf1311e5
armoney7 <112188406+armoney7@users.noreply.github.com>
2023-02-26 17:02:05 +0700

    Семинар 4 Задние 1

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Function(int a1, int b1)
+{
+    int comp = 1;
+    for (int step = 1; step <= b1; step++)
+    {
+        comp = comp * a1;
+    }
+    return comp;
+}
+int result = Function(a, b);
+Console.Write($"Число {a} в степени {b} = {result}");
