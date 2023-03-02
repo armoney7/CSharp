98772e1ad483a0d72f6466ddabe097fca4ae1e54
armoney7 <112188406+armoney7@users.noreply.github.com>
2023-03-03 00:01:58 +0700

    Seminar 6 Task 1

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 0; i < num; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void Decision(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) count += 1;
+    }
+    Console.Write($"{count} - такое количество чисел больше 0");
+}
+
+Decision(array);
